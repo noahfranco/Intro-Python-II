15e58fa8ea2da788a165f1358caa6b1a4be49dcb
noahfranco <noahfra@gmail.com>
2020-01-16 00:52:05 -0500

    made classes for player and room

--- src/adv.py
+++ src/adv.py
@@ -38,14 +38,14 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+# 
+# # Write a loop that:
+# #
+# # * Prints the current room name
+# # * Prints the current description (the textwrap module might be useful here).
+# # * Waits for user input and decides what to do.
+# #
+# # If the user enters a cardinal direction, attempt to move to the room there.
+# # Print an error message if the movement isn't allowed.
+# #
+# # If the user enters "q", quit the game.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+    def room_function(self):
+        print(self.name, self.description)
+        
+class Direction(Room):
+    def __init__(self, n_to, s_to, e_to, w_to, name, description):
+        super().__init__(name, description)
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+
+    def direction_function(self):
+        print(self.n_to, self.s_to, self.e_to, self.w_to)
